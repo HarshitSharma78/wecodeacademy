a30706b82dac5095b8258c2044f097af2cf16251
HarshitSharma78 <harshitsharma6373@gmail.com>
2023-02-15 22:30:29 +0530

    test file

---
+++ test-1.js
@@ -0,0 +1,73 @@
+
+//Q.1.....
+
+// let a = Number(prompt("How many french fries"))
+// let b = Number(prompt(" How many burgers "));
+// let c = Number(prompt(" How many plates of chowmin "));
+// let d = Number(prompt("How many plates of Manchurian "))
+// let e = Number(prompt("How many Cokes "))
+
+// let frenchfries = a * 60
+// let burgers = b *50 
+// let chowmin = c *100 
+// let Manchurian = d *80 
+// let Cokes = e * 50 
+
+// let total = frenchfries + burgers + chowmin + Manchurian + Cokes ;
+
+
+// console.log( `frenchfries : ${a} ; ${frenchfries}
+// burgers : ${b} ; ${burgers}
+// chowmin :${c} ; ${chowmin}
+// Manchurian :${d} ; ${Manchurian}
+// Cokes : ${e} ; ${Cokes}
+// total : ${total}
+// `  )
+
+
+//Q.2......
+
+
+// for ( let a = 5 ; a >= 1 ; a-- ){
+//   let ans = "";
+//   for( let i = 1 ; i <= a ; i++){
+//     ans = ans + "$##$"
+    
+//   }
+//   console.log(ans)
+// }
+
+
+//Q.10.......
+
+
+// let a = "naam"
+// if(a . length > 10){
+//   console.log(string.length)
+// }else{
+//   console.log("no")
+// }
+
+
+//Q.5........
+
+let a = Number(prompt("What is your birth year"))
+
+if ( a >= 18 ){
+  alert("Do you have valid license number?")
+  alert("User with license number #### is ready to drive."")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
